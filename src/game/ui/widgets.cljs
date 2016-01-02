614927b7d9ef28b90ebd779840f6c755c9348f5a
Joe Frangoudes <jfrangoudes@gmail.com>
2016-01-02 14:03:24 +0000

    fixed misplaced :p tag

--- src/game/ui/widgets.cljs
+++ src/game/ui/widgets.cljs
@@ -1,7 +1,7 @@
 (ns game.ui.widgets)
 
 (defn item-slot [item] (let [itemImg (item :src) itemName (item :name)] 
-  [:div [:img {:src itemImg} :p itemName]]))
+  [:div [:img {:src itemImg}] [:p itemName]]))
 
 (defn item-list [items]
   [:li (map (fn [item] [item]) items)])
