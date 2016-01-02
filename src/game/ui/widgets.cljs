adb4811d80207e2715164afbca31f50dbc45fa17
danprince <danielprince@hotmail.co.uk>
2016-01-02 20:12:16 +0530

    adds xp bar widget

--- src/game/ui/widgets.cljs
+++ src/game/ui/widgets.cljs
@@ -6,3 +6,16 @@
 (defn item-list [items]
   [:li (map (fn [item] [item]) items)])
 
+(defn xp-orb [k lit?]
+  (let [color (if lit? "blue" "grey")]
+    [:span {:key k :style {:color color }} "●"]))
+
+(defn xp-bar [max-xp current]
+  (let [empty-orbs (- max-xp current)]
+    [:div
+     [:strong "XP"]
+      [:span
+       (map-indexed xp-orb (repeat current true))]
+      [:span
+       (map-indexed xp-orb (repeat empty-orbs false))]]))
+
