92ba316634b94ca5310a172ccf897a41c4de5418
Joe Frangoudes <jfrangoudes@gmail.com>
2016-01-08 00:00:55 +0000

    Battle.cljs reads strength and defence from weapon and armour values monsters now have a weapon and some armour

--- src/game/procedural/monster.cljs
+++ src/game/procedural/monster.cljs
@@ -1,5 +1,8 @@
 (ns game.procedural.monster
-  (:require [game.models.monsters :as monsters]))
+  (:require [game.models.monsters :as monsters]
+            [game.models.items :as items]))
 
 (defn generate []
-  (rand-nth monsters/all))
+  (let [monster (rand-nth monsters/all)]
+    (assoc monster :weapon (rand-nth items/weapons)
+           :armour (rand-nth items/armor))))
